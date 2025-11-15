58f1f9b64f641e6f40f0e29935573d10a5a3bb35
elskussa <111130416+elskussa@users.noreply.github.com>
2025-11-15 05:50:15 +0100

    Add files via upload

---
+++ backUp/backUp-0.0/explicaciones/index.js
@@ -0,0 +1,13 @@
+const x = 2;
+const y = 2;
+let a 
+
+console.log("se crean las variables x e y, que son los lados de el cuadrado o los ejes de la gráfica");
+console.log("se crea la variable a")
+console.log("encontrar el espacio exácto que recorre a, que es la diagonal de x e y, que a su vez es el cateto de uno de los triangulos que conforman el triangulo mayor (x,(0,0),y");
+console.log("ejemplo visual");
+console.log(`||||2px\n||||\n2px`);
+console.log("operación final");
+a = x / Math.sqrt(2) ;
+
+console.log(a)
